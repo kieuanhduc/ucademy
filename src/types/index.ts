f2327da1bae2f9d9fe9701c85ce685ffcf2094e7
AndrewDuc <andrewduc2722@gmail.com>
2024-06-17 14:24:35 +0700

    update export type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,18 @@
-export type TActiveLinkProps = {
+type TActiveLinkProps = {
     url: string;
     children: React.ReactNode;
   };
-  export type TMenuItem = {
+type TMenuItem = {
     url: string;
     title: string;
     icon: React.ReactNode;
   };
-  // User
-  export type TCreateUserParams = {
+// User
+type TCreateUserParams = {
     clerkId: string;
     username: string;
     email_address: string;
     name?: string;
     avatar?: string;
-  };
-  export { TActiveLinkProps, TCreateUserParams, TMenuItem };
+};
+export type { TActiveLinkProps, TCreateUserParams, TMenuItem };
