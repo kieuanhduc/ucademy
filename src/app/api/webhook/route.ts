2d9172e399919fe104e2eb4f2afc34b38cd59959
AndrewDuc <andrewduc2722@gmail.com>
2024-06-17 14:49:23 +0700

    update code

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,48 +1,44 @@
-import { Webhook } from "svix";
-import { buffer } from "micro";
-import { WebhookEvent } from "@clerk/nextjs/server";
 import createUser from "@/lib/actions/user.actions";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-}
+import { Webhook } from "svix";
 
 
 export async function POST(req: Request) {
-    
-    const svix_id = req.headers.get("svix-id") ?? "";
-    const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
-    const svix_signature = req.headers.get("svix-signature") ?? "";
+  const svix_id = headers().get("svix-id") ?? "";
+  const svix_timestamp = headers().get("svix-timestamp") ?? "";
+  const svix_signature = headers().get("svix-signature") ?? "";
+  if (!process.env.WEBHOOK_SECRET) {
+    throw new Error("WEBHOOK_SECRET is not set");
+  }
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Bad Request", { status: 400 });
+  }
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
-    if (!process.env.WEBHOOK_SECRET) {
-        throw new Error("WEBHOOK_SECRET is not set");
-      }
+  const sivx = new Webhook(process.env.WEBHOOK_SECRET);
 
-    const payload = await req.json();
-    const body = JSON.stringify(payload);
+  let msg: WebhookEvent;
 
-    const sivx = new Webhook(process.env.WEBHOOK_SECRET);
-  
-    let msg:WebhookEvent;
-    
-    try {
-      msg = sivx.verify(body, {
-        "svix-id": svix_id,
-        "svix-timestamp": svix_timestamp,
-        "svix-signature": svix_signature,
-      }) as WebhookEvent;
-    } catch (err) {
-      return new Response("Bad Request", { status: 400 });
-    }
-  
-   const eventType = msg.type;
-   
-   if (eventType === "user.created") {
+  try {
+    msg = sivx.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  const eventType = msg.type;
+  if (eventType === "user.created") {
     // create user to database
     const { id, username, email_addresses, image_url } = msg.data;
+
+    console.log(msg);
+    
     const user = await createUser({
       username: username!,
       name: username!,
@@ -55,8 +51,6 @@ export async function POST(req: Request) {
       user,
     });
   }
-  
-    // Rest
-  
-    return new Response("OK", { status: 200 });
-  }
+
+  return new Response("OK", { status: 200 });
+}
